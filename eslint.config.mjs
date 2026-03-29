39fa5e69a597db9d7b46214e45027a1e44d81eb5
Archit0204 <amittal0204@gmail.com>
2026-03-29 21:10:15 +0530

    lenient linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,29 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // TypeScript
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-require-imports": "warn",
+
+      // React / Next.js
+      "react/no-unescaped-entities": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/rules-of-hooks": "warn",
+      "@next/next/no-img-element": "warn",
+      "@next/next/no-html-link-for-pages": "warn",
+
+      // General
+      "no-unused-vars": "off", // handled by @typescript-eslint
+      "prefer-const": "warn",
+      "no-console": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
